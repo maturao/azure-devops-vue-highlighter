61d5d8cba7a994199d9a722fe809597af6d7caed
maturao <ondramaturacze@gmail.com>
2025-07-16 00:16:17 +0200

    Add icon and update info

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-vue"],
+  modules: ["@wxt-dev/module-vue", "@wxt-dev/auto-icons"],
   manifest: {
+    name: "Azure DevOps Vue Highlighter",
     permissions: ["tabs", "storage"],
+  },
+  autoIcons: {
+    baseIconPath: "assets/icon.svg",
+    grayscaleOnDevelopment: false,
   },
 });
